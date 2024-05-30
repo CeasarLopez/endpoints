c59588b079bd99f08bd813694c0c698697b14f1b
Ceasar Lopez <155122341+CeasarLopez@users.noreply.github.com>
2024-05-30 01:22:11 +0000

    endpoint

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People, Planets, Favorites
 #from models import Person
 
 app = Flask(__name__)
@@ -40,10 +40,37 @@ def sitemap():
 def handle_hello():
 
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        "msg": "User"
     }
 
     return jsonify(response_body), 200
+# # Planets
+# # get all planets
+@app.route('/Planets', methods=['GET'])
+def handle_get_all_planets():
+    return "Hello Planets"
+# get one planet
+@app.route('/planets<int:planet_id', methods=['PUT','GET'])
+def handle_get_one_planet(planet_id):
+    if request.method == 'GET':
+        planet1 = Planets.query.get(planet_id)
+        return jsonify(planet1.serialize()), 200
+    return "Planet not found", 404
+# people
+# get all people
+@app.route('/people', methods=['GET'])
+def handle_get_all_people():
+    all_people= People.query.all()
+    serialized_people = [person.serialize()for person in all_people]
+    return jsonify(serialized_people), 200
+# get one person
+@app.route('/people<int:person_id>', methods=['PUT','GET'])
+def handle_get_one_person(person_id):
+    if request.method == 'GET':
+        user1 = People.query.get(person_id)
+        return jsonify(user1.serialize()), 200
+    return "Person not found", 404
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,15 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import ForeignKey
 
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
-    def __repr__(self):
-        return '<User %r>' % self.username
 
     def serialize(self):
         return {
@@ -17,3 +16,53 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Favorites(db.Model):
+    __tablename__ = "favorites"
+    id = db.Column(db.Integer, primary_key=True)
+    planet_id = db.Column(db.Integer, ForeignKey('planets.id'), nullable=True)
+    people_id = db.Column(db.Integer, ForeignKey('people.id'), nullable=True)
+    user_id = db.Column(db.Integer, nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "Planets_id": self.Planets_id,
+            "People_id" : self.People_id,
+            "User_id" : self.User_id
+        }
+
+class People(db.Model):
+    __tablename__ = "people"
+    id = db.Column(db.Integer, primary_key=True)
+    Name = db.Column(db.String(120), unique=True, nullable=False)
+    Height = db.Column(db.String(80), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    Favorites= db.relationship("Favorites", backref="People")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "Name_id": self.Name_id,
+            "Height_id": self.Height_id  
+        }
+class Planets(db.Model):
+    __tablename__ = "planets"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    diameter = db.Column(db.String(80), unique=False, nullable=False)
+    population = db.Column(db.String(80), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    Favorites= db.relationship("Favorites", backref="Planets")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name_id": self.name_id,
+            "diameter_id": self.diameter_id,
+            "population_id": self.population_id
+        }
+
+    def __repr__(self):
+
+        return '<User %r>' % self.username
